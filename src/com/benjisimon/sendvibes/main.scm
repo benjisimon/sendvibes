33dee42c457fe5556d62f13c34ef334fbca9266e
Ben Simon <ben@ideas2executables.com>
2014-11-07 00:58:39 -0500

    Just experimenting

--- src/com/benjisimon/sendvibes/main.scm
+++ src/com/benjisimon/sendvibes/main.scm
@@ -4,20 +4,25 @@
 
 (require 'android-defs)
 
-(define (resource path)
-  (let loop ((path path) (obj R))
-    (cond ((null? path) obj)
-          (else
-           (loop (cdr path) (static-field obj (car path)))))))
+(define (logi . entries)
+  (for-each (lambda (e) 
+              (android.util.Log:i "com.benjisimon.sendvibes" e))
+            entries))
+
+(define (resource type instance)
+  (logi "from resource: " (static-field R type))
+  (logi "going to fail: " (static-field (static-field R type) instance)))
 
 (activity
  main
+
  ((on-create-options-menu (menu :: android.view.Menu))
   (let ((inflator ((this):get-menu-inflater)))
     (inflator:inflate (static-field (static-field R 'menu) 'main) menu)
     #t))
                          
  (on-create-view 
-;  (as integer (resource '(layout main)))))
+  (logi "from main: " (static-field R 'layout))
+  (resource 'layout 'main)
   (static-field (static-field R 'layout) 'main)))
                  
