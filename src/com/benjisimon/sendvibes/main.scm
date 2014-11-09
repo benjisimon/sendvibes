58f2aa1df5405ab9cdbf145e5a8580c36ce8c5a6
Ben Simon <ben@ideas2executables.com>
2014-11-09 10:07:29 -0500

    better method for accessing resources

--- src/com/benjisimon/sendvibes/main.scm
+++ src/com/benjisimon/sendvibes/main.scm
@@ -9,20 +9,14 @@
               (android.util.Log:i "com.benjisimon.sendvibes" e))
             entries))
 
-(define (resource type instance)
-  (logi "from resource: " (static-field R type))
-  (logi "going to fail: " (static-field (static-field R type) instance)))
-
 (activity
  main
 
  ((on-create-options-menu (menu :: android.view.Menu))
   (let ((inflator ((this):get-menu-inflater)))
-    (inflator:inflate (static-field (static-field R 'menu) 'main) menu)
+    (inflator:inflate R$menu:main menu)
     #t))
                          
  (on-create-view 
-  (logi "from main: " (static-field R 'layout))
-  (resource 'layout 'main)
-  (static-field (static-field R 'layout) 'main)))
+  R$layout:main))
                  
